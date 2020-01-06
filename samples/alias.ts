cedc2b8aa9d7e3674fa8d189ef2abaf6202c9c98
Aguirre <Adrian Aguirre>
2020-01-06 15:40:01 -0800

    Stop ALias

---
+++ samples/alias.ts
@@ -0,0 +1,11 @@
+// Create a name for an existing type.
+
+type Alias1 = string | string[] | null;
+
+type Alias2 = { a: number } & { b: number };
+
+// Use aliases to describe complex types.
+type Alias4 = {
+  a: number;
+  b: number;
+};
